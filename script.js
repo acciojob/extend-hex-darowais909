67e9f02723c5bd8b233cc684a5baca2b323190ae
Owayas Ahmad Dar <144412066+darowais909@users.noreply.github.com>
2023-12-13 23:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if(shortHex.length===4){
+		let a=`#${shortHEX[1]}${shortHEX[1]}${shortHEX[2]}${shortHEX[2]}${shortHEX[3]}${shortHEX[3]}`
+		  console.log(a);
+	}
+	else{
+		let a=`#${shortHEX[0]}${shortHEX[0]}${shortHEX[1]}${shortHEX[1]}${shortHEX[2]}${shortHEX[2]}`
+		 console.log(a);
+	}
 };
 
 // Do not change the code below.
